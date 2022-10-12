dcbc1fd8c647557177a541b65ab0ca0380bf9a1a
remqes <remqes@gmail.com>
2022-10-12 19:34:19 +0200

    Zadanie 1 - wersja 1 + poprawka

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -11,7 +11,7 @@ public class Zadanie1 {
 
 		try{
 			BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-			for(int i=0; i<100; i++) writer.write(Fibonacci(i).toString() + "\n");
+			for(int i=0; i<=100; i++) writer.write(Fibonacci(i).toString() + "\n");
 			writer.close();
 		} catch(IOException ex) {
 			System.out.println("Błąd"); 
